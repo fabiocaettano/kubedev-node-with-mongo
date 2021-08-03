474b8f6163e38b7e4931465dbef524db532fb31d
fabiocaettano <fabio.caettano74@gmail.com>
2021-08-02 23:08:03 -0300

    Criação do arquivo server.js e src>route.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const path = require('path')
+
+const routes = require('./src/routes')
+
+const app = express()
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3003, function(){
+    console.log("Servidor iniciado com sucesso.")
+})
