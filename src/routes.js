1b735e4947899621be7898d6ce46ae9281e30053
fabiocaettano <fabio.caettano74@gmail.com>
2021-08-05 21:12:53 -0300

    Atualizar routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const UsuarioController = require('./controller/UsuarioController');
 
 const routes = express.Router();
 
@@ -6,8 +7,7 @@ routes.get('/',function(req,res){
     res.json({message: "Bem vindo ao Backend MongoDb"})
 })
 
-routes.get('/usuario',function(req,res){
-    res.json({message: "Bem vindo ao Backend MongoDb"})
-})
+routes.get('/usuario', UsuarioController.index)
+routes.post('/usuario', UsuarioController.store)
 
 module.exports = routes;
